ae846c25f52fb79982c75585c3260ae2cd056bf7
fenglin <huaiyinnianshao28@163.com>
2018-10-08 17:23:03 +0800

    成功发送带有附件的邮箱

--- mail/src/main/java/com/example/demo/MailDemoApplication.java
+++ mail/src/main/java/com/example/demo/MailDemoApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 测试详情见 test模块的单元测试。
+ */
 @SpringBootApplication
 public class MailDemoApplication {
 
